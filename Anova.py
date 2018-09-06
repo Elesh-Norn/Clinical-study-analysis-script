13e98196780b472842f80d0366643e61f28f1032
LDanaee <kercyan@live.fr>
2018-09-06 17:01:58 +0200

    Attempt to used stats models mixed model

--- Anova.py
+++ Anova.py
@@ -8,6 +8,9 @@ import pandas as pd
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from analysis_script import final_db
+
 from scipy import stats
 
 
@@ -28,4 +31,23 @@ def one_way_anova(data, parameter):
     table = sm.stats.anova_lm(data_lm, typ=2)
     # Type 2 ANOVA DataFrame
     return(table)
-    
+
+def get_simple_df(df, parameter1, parameter2):
+    # Create a df with only pairs
+    df2 = pd.DataFrame()
+    for elements in list(['Patient ID', 'Arm', 'Time point', parameter1, parameter2]):
+        df2[elements] = df[elements]
+    df2 = df2.dropna()
+    return df2
+
+
+data = final_db
+data = get_simple_df(data, 'Weight', 'Hospital')
+data = data.rename(columns={'Time point': 'Time_point'})
+data = data.rename(columns={'Patient ID': 'Patient_ID'})
+
+vc = {'Hospital': '0 + C(Hospital)'}
+md = smf.mixedlm("Weight ~ Time_point", data, groups=data['Arm'], vc_formula=vc)
+mdf = md.fit()
+
+print(mdf.summary())
